2706f3b28707583d7a5315d07e6c2c7f1699fa08
Tijmen <30957@ma-web.nl>
2020-09-17 10:53:20 +0200

    Smiley

---
+++ smiley.py
@@ -0,0 +1,33 @@
+import turtle 
+  
+pen = turtle.Turtle() 
+  
+def eye(col, rad): 
+    pen.down() 
+    pen.fillcolor(col) 
+    pen.begin_fill() 
+    pen.circle(rad) 
+    pen.end_fill() 
+    pen.up() 
+  
+  
+pen.fillcolor('blue') 
+pen.begin_fill() 
+pen.circle(100) 
+pen.end_fill() 
+pen.up() 
+pen.goto(-40, 120) 
+eye('white', 15) 
+pen.goto(-37, 125) 
+eye('black', 5) 
+pen.goto(40, 120) 
+eye('white', 15) 
+pen.goto(40, 125) 
+eye('black', 5) 
+pen.goto(-40, 85) 
+pen.down() 
+pen.right(90) 
+pen.circle(40, 180) 
+pen.up() 
+pen.end_fill() 
+pen.hideturtle() 
