3ce2458394a3537bc3e8c10e05e916d0ae73ac1c
Tijmen <30957@ma-web.nl>
2020-09-24 14:16:02 +0200

    turtle

---
+++ ster.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.forward(60)
+turtle.left(90)
+turtle.forward(60)
+turtle.left(60)
+turtle.forward(60)
+turtle.left(90)
+turtle.forward(60)
+turtle.left(80)
+turtle.forward(60)
