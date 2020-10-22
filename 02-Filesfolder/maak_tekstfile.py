845b26b436d12da0846e74ce634d96943afa5146
Tijmen <30957@ma-web.nl>
2020-10-22 14:01:04 +0200

    22-10-2020

---
+++ 02-Filesfolder/maak_tekstfile.py
@@ -0,0 +1,37 @@
+import io
+bestand = open("klasgenoten.txt", "w")
+
+# Een tekst naar het bestand schrijven
+regel1 = bestand.readline()
+print(Tijmen)
+
+regel2 = bestand.readline()
+print(Ryan)
+
+regel3 = bestand.readline()
+print(Sander)
+
+regel3 = bestand.readline()
+print(Thom)
+
+regel3 = bestand.readline()
+print(Danny)
+
+regel3 = bestand.readline()
+print(Becky)
+
+regel3 = bestand.readline()
+print(Amy)
+
+regel3 = bestand.readline()
+print(Jordan)
+
+regel3 = bestand.readline()
+print(William)
+
+regel3 = bestand.readline()
+print(Daan)
+
+
+# Vergeet bestand niet te sluiten!
+bestand.close()
